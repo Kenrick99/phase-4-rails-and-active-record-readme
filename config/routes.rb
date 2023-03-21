8569931d2af359aaf3d633ccf31df8401d8f0428
kenrick <kenrickdony@gmail.com>
2023-03-21 13:15:24 +0300

    completed lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+class Cheese < ApplicationRecord
+  def summary
+      "#{self.name}: $#{self.price}"
+  end 
 end
